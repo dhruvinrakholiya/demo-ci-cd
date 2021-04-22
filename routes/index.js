d6bf7168b5a661283a0b9007028ae502e31cc283
mac <mac@macs-iMac.local>
2021-04-22 14:18:56 +0530

    add route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/info', function(req, res, next) {
+  return res.json({message:"Welcome info route"})
+});
 
 module.exports = router;
