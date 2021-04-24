3270a7de17dbde532c57b43de9d4c9c1bf677b18
mac <mac@macs-iMac.local>
2021-04-24 10:53:33 +0530

    add api

--- functions/routes/users.js
+++ functions/routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/info', function(req, res, next) {
+  res.json({name:"Dhruvin",age:21});
+});
 
 module.exports = router;
